f74667063be67b719f2e9839552ecd23cd4b6555
Pedreli <felipepedreli@gmail.com>
2019-10-18 19:50:32 -0300

    Altera menu de perfil

--- client/src/containers/DefaultLayout/DefaultHeader.js
+++ client/src/containers/DefaultLayout/DefaultHeader.js
@@ -45,12 +45,6 @@ class DefaultHeader extends Component {
           <NavItem className="d-md-down-none">
             <NavLink to="#" className="nav-link"><i className="icon-bell"></i><Badge pill color="danger">5</Badge></NavLink>
           </NavItem>
-          <NavItem className="d-md-down-none">
-            <NavLink to="#" className="nav-link"><i className="icon-list"></i></NavLink>
-          </NavItem>
-          <NavItem className="d-md-down-none">
-            <NavLink to="#" className="nav-link"><i className="icon-location-pin"></i></NavLink>
-          </NavItem>
           <UncontrolledDropdown nav direction="down">
             <DropdownToggle nav>
               <img src={'../../assets/img/avatars/1.jpg'} className="img-avatar" />
@@ -58,14 +52,10 @@ class DefaultHeader extends Component {
             <DropdownMenu right>
               <DropdownItem header tag="div" className="text-center"><strong>Perfil</strong></DropdownItem>
               <DropdownItem><i className="fa fa-envelope-o"></i> Mensagens<Badge color="success">42</Badge></DropdownItem>
-              <DropdownItem><i className="fa fa-tasks"></i> Tarefas<Badge color="danger">42</Badge></DropdownItem>
               <DropdownItem><i className="fa fa-comments"></i> Comentários<Badge color="warning">42</Badge></DropdownItem>
               <DropdownItem header tag="div" className="text-center"><strong>Configurações</strong></DropdownItem>
               <DropdownItem><i className="fa fa-user"></i> Meu Perfil</DropdownItem>
               <DropdownItem><i className="fa fa-wrench"></i> Ajustes</DropdownItem>
-              <DropdownItem><i className="fa fa-usd"></i> Promoções<Badge color="secondary">42</Badge></DropdownItem>
-              <DropdownItem><i className="fa fa-file"></i> Projetos<Badge color="primary">42</Badge></DropdownItem>
-              <DropdownItem divider />
               <DropdownItem><i className="fa fa-shield"></i>Segurança</DropdownItem>
               <DropdownItem onClick={e => this.props.onLogout(e)}><i className="fa fa-lock"></i> Sair</DropdownItem>
             </DropdownMenu>
