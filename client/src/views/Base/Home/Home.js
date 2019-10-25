b9e7a509beefe6eceaeb7299bb8bcc30ce3b6c19
Pedreli <felipepedreli@gmail.com>
2019-10-25 19:46:59 -0300

    Finaliza tela de post

--- client/src/views/Base/Home/Home.js
+++ client/src/views/Base/Home/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
       <div className="animated fadeIn">
         <Card>
           <CardHeader>
-            <i className="fa fa-align-justify"></i><strong>Novo Post</strong>
+            <strong>Novo Post</strong>
           </CardHeader>
             <CardBody>
               <Editor
@@ -22,67 +22,68 @@ class Home extends Component {
               />
             </CardBody>
         </Card>
-        <Row>
-          <Col>
-            <Card>
-              <CardHeader>
-                <i className="fa fa-align-justify"></i><strong>Autor: Felipe</strong>
-              </CardHeader>
-              <CardBody>
-                <Jumbotron>
-                  <h1 className="display-3">Post 1!</h1>
-                  <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra
-                    attention to featured content or information.</p>
-                  <hr className="my-2" />
-                  <p>It uses utility classes for typgraphy and spacing to space content out within the larger container.</p>
-                  <p className="lead">
-                    <Button color="primary">Detalhes</Button>
-                  </p>
-                </Jumbotron>
-              </CardBody>
-            </Card>
-          </Col>
 
-          <Col>
-            <Card>
-              <CardHeader>
-                <i className="fa fa-align-justify"></i><strong>Autor: Bruno</strong>
-              </CardHeader>
-              <CardBody>
-                <Jumbotron>
-                  <h1 className="display-3">Post 2!</h1>
-                  <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra
-                    attention to featured content or information.</p>
-                  <hr className="my-2" />
-                  <p>It uses utility classes for typgraphy and spacing to space content out within the larger container.</p>
-                  <p className="lead">
-                    <Button color="primary">Detalhes</Button>
-                  </p>
-                </Jumbotron>
-              </CardBody>
-            </Card>
-          </Col>
+          <Row>
+            <Col>
+              <Card>
+                <CardHeader>
+                  <strong>Autor: Felipe</strong>
+                </CardHeader>
+                <CardBody>
+                  <Jumbotron>
+                    <h1 className="display-3">Post 1!</h1>
+                    <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra
+                      attention to featured content or information.</p>
+                    <hr className="my-2" />
+                    <p>It uses utility classes for typgraphy and spacing to space content out within the larger container.</p>
+                    <p className="lead">
+                      <Button color="primary">Detalhes</Button>
+                    </p>
+                  </Jumbotron>
+                </CardBody>
+              </Card>
+            </Col>
 
-          <Col>
-            <Card>
-              <CardHeader>
-                <i className="fa fa-align-justify"></i><strong>Autor: Guilherme</strong>
-              </CardHeader>
-              <CardBody>
-                <Jumbotron>
-                  <h1 className="display-3">Post 3!</h1>
-                  <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra
-                    attention to featured content or information.</p>
-                  <hr className="my-2" />
-                  <p>It uses utility classes for typgraphy and spacing to space content out within the larger container.</p>
-                  <p className="lead">
-                    <Button color="primary">Detalhes</Button>
-                  </p>
-                </Jumbotron>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
+            <Col>
+              <Card>
+                <CardHeader>
+                  <i className="fa fa-align-justify"></i><strong>Autor: Bruno</strong>
+                </CardHeader>
+                <CardBody>
+                  <Jumbotron>
+                    <h1 className="display-3">Post 2!</h1>
+                    <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra
+                      attention to featured content or information.</p>
+                    <hr className="my-2" />
+                    <p>It uses utility classes for typgraphy and spacing to space content out within the larger container.</p>
+                    <p className="lead">
+                      <Button color="primary">Detalhes</Button>
+                    </p>
+                  </Jumbotron>
+                </CardBody>
+              </Card>
+            </Col>
+
+            <Col>
+              <Card>
+                <CardHeader>
+                  <i className="fa fa-align-justify"></i><strong>Autor: Guilherme</strong>
+                </CardHeader>
+                <CardBody>
+                  <Jumbotron>
+                    <h1 className="display-3">Post 3!</h1>
+                    <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra
+                      attention to featured content or information.</p>
+                    <hr className="my-2" />
+                    <p>It uses utility classes for typgraphy and spacing to space content out within the larger container.</p>
+                    <p className="lead">
+                      <Button color="primary">Detalhes</Button>
+                    </p>
+                  </Jumbotron>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
       </div>
     );
   }
