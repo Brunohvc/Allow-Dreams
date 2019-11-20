d9a33a79e12a7b738c31f1936e8baaa6f359d17b
Pedreli <felipepedreli@gmail.com>
2019-11-20 08:34:43 -0300

    Cria tela de perfil de usuário

--- client/src/views/Perfil/Perfil.js
+++ client/src/views/Perfil/Perfil.js
@@ -1,26 +1,200 @@
-import React, { Component } from 'react';
-import { Card, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
+import React from "react";
 
-class Perfil extends Component {
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  FormGroup,
+  Form,
+  Input,
+  Row,
+  Col
+} from "reactstrap";
 
+class Perfil extends React.Component {
   render() {
     return (
-      <div className="animated fadeIn">
-        <Row>
-          <Col>
-            <Card>
-              <CardHeader>
-                <strong> Fazer tela de usuário </strong>
-              </CardHeader>
-              <CardBody>
-
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    )
+      <>
+        <div className="content">
+          <Row>
+            <Col md="4">
+              <Card className="card-user">
+                <div className="image">
+                  <img
+                    alt="..."
+                    src={require("../../assets/img/felipe2.jpeg")}
+                  />
+                </div>
+                <CardBody>
+                  <div className="author">
+                    <a href="#pablo" onClick={e => e.preventDefault()}>
+                      <img
+                        alt="..."
+                        className="avatar border-gray"
+                        src={require("../../assets/img/felipe2.jpeg")}
+                      />
+                      <h5 className="title">Felipe Pedreli</h5>
+                    </a>
+                    <p className="description">@felipepedreli</p>
+                  </div>
+                  <p className="description text-center">
+                    Eu gosto de futebol <br />
+                    Sou das festinhas <br />Sou programador!
+                  </p>
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="button-container">
+                    <Row>
+                      <Col className="ml-auto" lg="3" md="6" xs="6">
+                        <h5>
+                          12 <br />
+                          <small>Fotos</small>
+                        </h5>
+                      </Col>
+                      <Col className="mr-auto" lg="3">
+                        <h5>
+                          9.570<br />
+                          <small>Seguidor</small>
+                        </h5>
+                      </Col>
+                    </Row>
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+            <Col md="8">
+              <Card className="card-user">
+                <CardHeader>
+                  <CardTitle tag="h5">Editar Perfil</CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <Form>
+                    <Row>
+                      <Col className="pr-1" md="5">
+                        <FormGroup>
+                          <label>Empresa</label>
+                          <Input
+                            defaultValue="Empresa onde trabalha."
+                            placeholder="Company"
+                            type="text"
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col className="px-1" md="3">
+                        <FormGroup>
+                          <label>Nome Perfil</label>
+                          <Input
+                            defaultValue="User"
+                            placeholder="Usuário"
+                            type="text"
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col className="pl-1" md="4">
+                        <FormGroup>
+                          <label htmlFor="ExemploEmail">
+                            Email
+                          </label>
+                          <Input placeholder="Email" type="email" />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="pr-1" md="6">
+                        <FormGroup>
+                          <label>Primeiro Nome</label>
+                          <Input
+                            defaultValue="Nome"
+                            placeholder="Primeiro Nome"
+                            type="text"
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col className="pl-1" md="6">
+                        <FormGroup>
+                          <label>Ultimo Nome</label>
+                          <Input
+                            defaultValue="Nome"
+                            placeholder="Ultimo Nome"
+                            type="text"
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md="12">
+                        <FormGroup>
+                          <label>Endereço</label>
+                          <Input
+                            defaultValue="Jaraguá do Sul, Brasil"
+                            placeholder="Endereço"
+                            type="text"
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="pr-1" md="4">
+                        <FormGroup>
+                          <label>Cidade</label>
+                          <Input
+                            defaultValue="Jaraguá do Sul"
+                            placeholder="Cidade"
+                            type="text"
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col className="px-1" md="4">
+                        <FormGroup>
+                          <label>Pais</label>
+                          <Input
+                            defaultValue="Brasil"
+                            placeholder="Pais"
+                            type="text"
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col className="pl-1" md="4">
+                        <FormGroup>
+                          <label>Codigo Postal</label>
+                          <Input placeholder="Código Postal" type="number" />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md="12">
+                        <FormGroup>
+                          <label>Sobre Mim</label>
+                          <Input
+                            type="textarea"
+                            defaultValue="Opa, gosto de praticar esportes e estar com os amigos..."
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <div className="update ml-auto mr-auto">
+                        <Button
+                          className="btn-round"
+                          color="primary"
+                          type="submit"
+                        >
+                          Atualizar Perfil
+                        </Button>
+                      </div>
+                    </Row>
+                  </Form>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </>
+    );
   }
 }
-
 export default Perfil;
