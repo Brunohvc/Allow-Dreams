078c70f4ce0e419670dfd0005020ba1675aa2643
Pedreli <felipepedreli@gmail.com>
2019-11-22 09:11:03 -0300

    Recria tela de editar perfil

--- client/src/views/EditPerfil/EditPerfil.js
+++ client/src/views/EditPerfil/EditPerfil.js
@@ -1,151 +1,188 @@
-import React from "react";
-import {
-  Button,
-  Card,
-  CardHeader,
-  CardBody,
-  CardTitle,
-  FormGroup,
-  Form,
-  Input,
-  Row,
-  Col
-} from "reactstrap";
+import React, { Component } from 'react';
+import { Button, Card, CardBody, CardFooter, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row, ListGroupItem } from 'reactstrap';
+import axios from 'axios';
+import swal from 'sweetalert';
+import { Link } from 'react-router-dom';
 
-class EditPerfil extends React.Component {
+class EditPerfil extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { name: '', phone: '', nickname: '', password: '', passwordC: '', error: [] };
+
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangePhone = this.handleChangePhone.bind(this);
+    this.handleChangeNickname = this.handleChangeNickname.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleChangePasswordC = this.handleChangePasswordC.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeName(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleChangePhone(event) {
+    this.setState({ phone: event.target.value });
+  }
+
+  handleChangeNickname(event) {
+    this.setState({ nickname: event.target.value });
+  }
+
+  handleChangePassword(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handleChangePasswordC(event) {
+    this.setState({ passwordC: event.target.value });
+  }
+
+  handleSubmit(event) {
+    let valid = true;
+    let newerros = [];
+
+    if (this.state.name.length < 3) {
+      newerros.push("O campo 'Nome' deve ter no mínimo 3 caracteres!")
+      valid = false;
+    }
+
+    if (this.state.phone.length < 3) {
+      newerros.push("O campo 'Telefone' não é válido!")
+      valid = false;
+    }
+
+    if (this.state.nickname.length < 3) {
+      newerros.push("O campo 'Login' deve ter no mínimo 3 caracteres!")
+      valid = false;
+    }
+
+    if (this.state.password.length < 3) {
+      newerros.push("O campo 'Senha' deve ter no mínimo 4 caracteres!")
+      valid = false;
+    }
+
+    if (this.state.passwordC.length != this.state.password.length) {
+      newerros.push("O campo 'Senha' e 'Repetir Senha' não coincidem!")
+      valid = false;
+    }
+
+    this.setState({ error: newerros });
+    if (valid) {
+
+      axios.post(`http://127.0.0.1:3333/api/v1/users`, {
+        "name": this.state.name,
+        "phone": this.state.phone,
+        "nickname": this.state.nickname,
+        "password": this.state.password
+      })
+        .then(function (response) {
+          if (response.data.message) {
+            swal("Erro!", response.data.message, "error");
+          } else {
+            localStorage.setItem('dadosUser', JSON.stringify(response.data))
+            window.location.hash = "#/home";
+          }
+        })
+        .catch(function (error) {
+          swal("Erro!", "Um erro inesperado ocorreu, tente novamente!", "error");
+        });
+    }
+  }
+
   render() {
     return (
-      <div className="content">
-        <Row>
-          <Col md="12">
-            <Card className="card-user">
-              <CardHeader>
-                <CardTitle tag="h5">Editar Dados de Usuário</CardTitle>
-              </CardHeader>
-              <CardBody>
-                <Form>
-                  <Row>
-                    <Col className="pr-1" md="4">
-                      <FormGroup>
-                        <label>Nickname</label>
-                        <Input
-                          defaultValue="Nickname"
-                          placeholder="Nickname"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="px-1" md="4">
-                      <FormGroup>
-                        <label>Nome Perfil</label>
-                        <Input
-                          defaultValue="Nome do Perfil"
-                          placeholder="Usuário"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="pl-1" md="4">
-                      <FormGroup>
-                        <label htmlFor="ExemploEmail">
-                          Email
-                        </label>
-                        <Input placeholder="Email" type="email" />
-                      </FormGroup>
-                    </Col>
-                  </Row>
+        <Container>
+          <Row className="justify-content-center">
+            <Col md="9" lg="7" xl="6">
+              <Card className="mx-4">
+                <CardBody className="p-4">
+                  <Form onSubmit={this.handleSubmit}>
+                    <h1>Dados de Usuário</h1>
+                    <p className="text-muted">Mude seus dados</p>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-user"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="text" placeholder="Seu Nome" key="name" autoComplete="name" value={this.state.name} onChange={this.handleChangeName} required />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="cui-phone icons"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="text" placeholder="Seu Telefone" key="phone" autoComplete="phone" value={this.state.phone} onChange={this.handleChangePhone} required />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>@</InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="text" placeholder="Nickname" key="nickname" autoComplete="login" value={this.state.nickname} onChange={this.handleChangeNickname} required />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-lock"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="password" placeholder="Senha Atual" key="password" autoComplete="new-password" value={this.state.password} onChange={this.handleChangePassword} required />
+                    </InputGroup>
+                    <InputGroup className="mb-4">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-lock"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="password" placeholder="Nova Senha" key="passwordC" autoComplete="new-password" value={this.state.passwordC} onChange={this.handleChangePasswordC} required />
+                    </InputGroup>
+                    <InputGroup className="mb-4">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-calendar"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="date" placeholder="Data de aniversário" key="aniversario" autoComplete="new-password" value={this.state.passwordC} onChange={this.handleChangePasswordC} required />
+                    </InputGroup>
+                    <InputGroup className="mb-4">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-people"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="select" placeholder="Gênero" key="genero" value={this.state.passwordC} onChange={this.handleChangePasswordC} required>
+                        <option>Masculino</option>
+                        <option>Feminino</option>
+                      </Input>
+                    </InputGroup>
+                    <Button color="success" block>Alterar dados</Button>
+                  </Form>
+                </CardBody>
+                {
+                  this.state.error.length > 0 &&
+                  <CardFooter className="p-4">
 
-                  <Row>
-                    <Col className="pr-1" md="4">
-                      <FormGroup>
-                        <label>Telefone</label>
-                        <Input
-                          defaultValue="Telefone."
-                          placeholder="Seu telefone"
-                          type="number"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="px-1" md="4">
-                      <FormGroup>
-                        <label>Data de aniversário</label>
-                        <Input
-                          defaultValue="Data"
-                          placeholder="Data de aniversário"
-                          type="date"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="px-1" md="4">
-                    <FormGroup>
-                    <label>Gênero</label>
-                    <Input 
-                      defaultValue="Gênero"
-                      placeholder="Gênero"
-                      type="select">
-                      <option>Masculino</option>
-                      <option>Feminino</option>
-                    </Input>
-                  </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Button
-                      style={{ marginLeft: '15px' }}
-                      className="btn-round"
-                      color="primary"
-                      type="submit"
-                    >
-                      Atualizar Perfil
-                    </Button>
-                  </Row>
-                </Form>
-              </CardBody>
-            </Card>
-
-            <Card className="card-user">
-              <CardHeader>
-                <CardTitle tag="h5">Editar Senha</CardTitle>
-              </CardHeader>
-              <CardBody>
-                <Form>
-                  <Row>
-                    <Col className="pr-1" md="4">
-                      <FormGroup>
-                        <label>Senha Atual</label>
-                        <Input
-                          placeholder="Digite sua senha atual"
-                          type="password"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="px-1" md="4">
-                      <FormGroup>
-                        <label>Nova Senha</label>
-                        <Input
-                          placeholder="Digite sua nova senha"
-                          type="password"
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Button
-                      style={{ marginLeft: '15px' }}
-                      className="btn-round"
-                      color="success"
-                      type="submit"
-                    >
-                      Mudar Senha
-                    </Button>
-                  </Row>
-                </Form>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </div>
+                    {
+                      this.state.error.map(function (erro) {
+                        return (
+                          <Row key={erro}>
+                            <Col xs="12" sm="12">
+                              <ListGroupItem action color="danger">{erro}</ListGroupItem>
+                            </Col>
+                          </Row>
+                        )
+                      })
+                    }
+                  </CardFooter>
+                }
+              </Card>
+            </Col>
+          </Row>
+        </Container>
     );
   }
 }
+
 export default EditPerfil;
