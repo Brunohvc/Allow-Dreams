3e72dd25e3ffb7f0ecdf780c5c900f78b67f91be
Pedreli <felipepedreli@gmail.com>
2019-11-26 14:48:10 -0300

    Remove itens não utilizados

--- client/src/views/Base/index.js
+++ client/src/views/Base/index.js
@@ -1,22 +1,11 @@
-import Breadcrumbs from './Breadcrumbs';
 import Cards from './Cards';
 import Carousels from './Carousels';
-import Collapses from './Collapses';
 import Dropdowns from './Dropdowns';
 import Forms from './Forms';
 import Home from './Home';
-import ListGroups from './ListGroups';
-import Navbars from './Navbars';
 import Navs from './Navs';
-import Popovers from './Popovers';
-import Paginations from './Paginations';
-import ProgressBar from './ProgressBar';
-import Switches from './Switches';
-import Tables from './Tables';
-import Tabs from './Tabs';
-import Tooltips from './Tooltips';
 
 export {
-  Breadcrumbs, Cards, Carousels, Collapses, Dropdowns, Forms, Home, ListGroups, Navbars, Navs, Popovers, ProgressBar, Switches, Tables, Tabs, Tooltips, Paginations
+  Cards, Carousels, Dropdowns, Forms, Home, Navs
 };
 
