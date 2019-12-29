a14fd2ca136cf3a28843fa57be3727450f745b3c
7Bryssk <bruninho.carvalho159@gmail.com>
2019-12-29 09:48:13 -0300

    teste deploy

--- client/src/views/Register/Register.js
+++ client/src/views/Register/Register.js
@@ -98,6 +98,7 @@ class Register extends Component {
               <Card className="mx-4">
                 <CardBody className="p-4">
                   <Form>
+                    
                     <h1>Cadastro</h1>
                     <p className="text-muted">Crie a sua conta</p>
                     <InputGroup className="mb-3">
